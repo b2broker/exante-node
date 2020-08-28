e28cdee75e0a4e1f5181f25a106d5c865322e26c
Sergey Bakulin <sbakulin@b2broker.net>
2020-08-28 14:33:50 +0300

    test: add getAccountSummary

--- test/rest.spec.ts
+++ test/rest.spec.ts
@@ -2,7 +2,15 @@ import assert from "assert";
 import nock from "nock";
 import fetch from "node-fetch";
 
-import { RestClient, ExanteDemoURL, ExanteLiveURL, FetchError } from "../";
+import {
+  RestClient,
+  ExanteDemoURL,
+  ExanteLiveURL,
+  FetchError,
+  IUserAccount,
+  IAccountSummary,
+  DefaultAPIVersion,
+} from "../";
 
 const client_id = "d0c5340b-6d6c-49d9-b567-48c4bfca13d2";
 const app_id = "6cca6a14-a5e3-4219-9542-86123fc9d6c3";
@@ -73,7 +81,7 @@ suite("RestClient", () => {
 
   test(".getAccounts()", async () => {
     const version = "3.0";
-    const response = [
+    const response: IUserAccount[] = [
       { status: "Full", accountId: "ABC1234.002" },
       { status: "Full", accountId: "ABC1234.001" },
     ];
@@ -85,16 +93,124 @@ suite("RestClient", () => {
   });
 
   test(".getAccounts() (with no arguments)", async () => {
-    const version = "2.0";
     const response = [
       { status: "Full", accountId: "ABC1234.002" },
       { status: "Full", accountId: "ABC1234.001" },
     ];
 
-    nock(url).get(`/md/${version}/accounts`).delay(1).reply(200, response);
+    nock(url)
+      .get(`/md/${DefaultAPIVersion}/accounts`)
+      .delay(1)
+      .reply(200, response);
 
     const accounts = await client.getAccounts();
     assert.deepStrictEqual(accounts, response);
+  });
+
+  test(".getAccountSummary()", async () => {
+    const version = "3.0";
+    const id = "ABC1234.001";
+    const date = "2013-02-16";
+    const currency = "EUR";
+    const response: IAccountSummary = {
+      freeMoney: "123406.01",
+      marginUtilization: "0.0",
+      account: "ABC1234.001",
+      currencies: [
+        {
+          convertedValue: "123456.01",
+          price: "123456.01",
+          code: "EUR",
+          value: "123456.01",
+        },
+      ],
+      netAssetValue: "123456.01",
+      currency: "EUR",
+      moneyUsedForMargin: "50.0",
+      positions: [
+        {
+          quantity: "1",
+          convertedValue: "133.1",
+          convertedPnl: "12.1",
+          id: "AAPL.NASDAQ",
+          currency: "USD",
+          price: "120.0",
+          symbolId: "AAPL.NASDAQ",
+          symbolType: "STOCK",
+          value: "110.0",
+          averagePrice: "110.0",
+          pnl: "10.0",
+        },
+      ],
+      timestamp: 1503619200000,
+      sessionDate: [2013, 2, 16],
+      accountId: "ABC1234.001",
+    };
+
+    nock(url)
+      .get(`/md/${version}/summary/${id}/${date}/${currency}`)
+      .delay(1)
+      .reply(200, response);
+
+    const summary = await client.getAccountSummary({
+      version,
+      id,
+      date,
+      currency,
+    });
+    assert.deepStrictEqual(summary, response);
+  });
+
+  test(".getAccountSummary() (with no version)", async () => {
+    const id = "ABC1234.001";
+    const date = "2013-02-16";
+    const currency = "EUR";
+    const response: IAccountSummary = {
+      freeMoney: "123406.01",
+      marginUtilization: "0.0",
+      account: "ABC1234.001",
+      currencies: [
+        {
+          convertedValue: "123456.01",
+          price: "123456.01",
+          code: "EUR",
+          value: "123456.01",
+        },
+      ],
+      netAssetValue: "123456.01",
+      currency: "EUR",
+      moneyUsedForMargin: "50.0",
+      positions: [
+        {
+          quantity: "1",
+          convertedValue: "133.1",
+          convertedPnl: "12.1",
+          id: "AAPL.NASDAQ",
+          currency: "USD",
+          price: "120.0",
+          symbolId: "AAPL.NASDAQ",
+          symbolType: "STOCK",
+          value: "110.0",
+          averagePrice: "110.0",
+          pnl: "10.0",
+        },
+      ],
+      timestamp: 1503619200000,
+      sessionDate: [2013, 2, 16],
+      accountId: "ABC1234.001",
+    };
+
+    nock(url)
+      .get(`/md/${DefaultAPIVersion}/summary/${id}/${date}/${currency}`)
+      .delay(1)
+      .reply(200, response);
+
+    const summary = await client.getAccountSummary({
+      id,
+      date,
+      currency,
+    });
+    assert.deepStrictEqual(summary, response);
   });
 
   suite("Static methods", () => {
