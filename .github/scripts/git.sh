1c043cf813eea43c074586748eb7de20e635cbca
Sergey Bakulin <sbakulin@b2broker.net>
2020-09-03 19:21:39 +0300

    chore: add .prettierignore

--- .github/scripts/git.sh
+++ .github/scripts/git.sh
@@ -3,7 +3,7 @@
 # Set pgp siging key
 git config user.signingkey ${PGP_KEY_ID}
 
-# Set the custom gpg program (that passes the passphrase to `gpg2`)
+# Set the custom gpg program (that passes the passphrase to `gpg`)
 git config gpg.program ./.github/scripts/gpg.sh
 
 # Sign commits with PGP key
