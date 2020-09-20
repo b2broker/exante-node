95b89784262013e1bb0a7a76b49888b6ca34c66e
Sergey Bakulin <sbakulin@b2broker.net>
2020-09-20 22:16:01 +0300

    feat: add the `JSONStream` class

---
+++ src/stream.ts
@@ -0,0 +1,25 @@
+import { Transform } from "stream";
+
+type Callback = (error: Error | null) => void;
+
+export class JSONStream extends Transform {
+  public constructor() {
+    super({ objectMode: true });
+  }
+
+  public _transform(chunk: Buffer, _encoding: string, callback: Callback) {
+    try {
+      const messages = chunk.toString("utf8").split("\n");
+
+      for (const message of messages) {
+        if (message) {
+          this.push(JSON.parse(message));
+        }
+      }
+
+      callback(null);
+    } catch (error) {
+      callback(error);
+    }
+  }
+}
