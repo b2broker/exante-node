fa4023dea67fec113f19ee53a71d6e9f73fbacbf
Sergey Bakulin <sbakulin@b2broker.net>
2021-05-31 21:40:25 +0300

    perf(JSONStream): add the `chunk` property to errors

--- src/stream.ts
+++ src/stream.ts
@@ -8,18 +8,22 @@ export class JSONStream extends Transform {
   }
 
   public _transform(chunk: Buffer, _encoding: string, cb: Callback): void {
-    const messages = chunk.toString("utf8").split("\n");
+    const text = chunk.toString("utf8");
+    const messages = text.split("\n");
+
     for (const message of messages) {
       if (message) {
         try {
           this.push(JSON.parse(message));
         } catch (error: unknown) {
           (error as { json_string?: string }).json_string = message;
+          (error as { chunk?: string }).chunk = text;
           cb(error as Error);
           return;
         }
       }
     }
+
     cb(null);
   }
 }
