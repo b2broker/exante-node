a07831fabff36a76be52f3e74e9c7dc732bf727a
Sergey Bakulin <sbakulin@b2broker.net>
2021-09-06 18:24:03 +0300

    fix(JSONStream): buffer incomplete chunks

--- src/stream.ts
+++ src/stream.ts
@@ -3,24 +3,36 @@ import { Transform } from "node:stream";
 type Callback = (error: Error | null) => void;
 
 export class JSONStream extends Transform {
+  #buffer: string;
+
   public constructor() {
     super({ objectMode: true });
+    this.#buffer = "";
   }
 
   public _transform(chunk: Buffer, _encoding: string, cb: Callback): void {
-    const text = chunk.toString("utf8");
-    const messages = text.split("\n");
+    const text = this.#buffer + chunk.toString("utf8");
+
+    this.#buffer = "";
+
+    const messages = text.split("\n").filter((s) => s);
+
+    if (!text.endsWith("\n")) {
+      const last_chunk = messages.pop();
+
+      if (last_chunk) {
+        this.#buffer = last_chunk;
+      }
+    }
 
     for (const message of messages) {
-      if (message) {
-        try {
-          this.push(JSON.parse(message));
-        } catch (error: unknown) {
-          (error as { json_string?: string }).json_string = message;
-          (error as { chunk?: string }).chunk = text;
-          cb(error as Error);
-          return;
-        }
+      try {
+        this.push(JSON.parse(message));
+      } catch (error: unknown) {
+        (error as { json_string?: string }).json_string = message;
+        (error as { chunk?: string }).chunk = text;
+        cb(error as Error);
+        return;
       }
     }
 
